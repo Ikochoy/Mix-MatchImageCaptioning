c8f5c6ca312ff7a36f7c07aa79ba2f78f9804edf
Ikochoy <kwan.choy@mail.utoronto.ca>
2022-04-03 00:02:29 -0400

    add apis to imageset class

--- Data/imageset.py
+++ Data/imageset.py
@@ -2,14 +2,24 @@ class ImageSet:
   def __init__(self, images={}):
     # Dictionary of images
     self.images = images
+    self.annotations = {}
+    self.files = {}
   
   def add_image(self,image_id, image):
     self.images[image_id] = image
+    self.annotations[image_id] = image.annotations
+    self.files[image_id] = image.file_name
   
   def get_image_by_id(self, image_id):
     return self.images[image_id]
   
-  def get_image_list(self):
-      pass
+  def get_annotation_list(self):
+    return list(self.annotations.values())
+
+  def get_image_ids(self):
+    return list(self.images.keys())
+  
+  def get_images(self):
+    return list(self.files.values())
 
 
