a89e551202ed5d1d48650bd274be8274513287d8
Ikochoy <kwan.choy@mail.utoronto.ca>
2022-04-11 19:38:56 -0400

    add changes to ImageEncoder

--- Models/ImageEncoder.py
+++ Models/ImageEncoder.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 class ImageEncoder(nn.Module):
   def __init__(self, choice, dropout=False, fine_tune=False):
     super(ImageEncoder, self).__init__()
-    self.input = CROPPED_WIDTH
+    # self.input = CROPPED_WIDTH
     # Detect if we have a GPU available
     self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     self.trained = False
@@ -25,35 +25,31 @@ class ImageEncoder(nn.Module):
     ])
     if choice == "GoogLeNet":
       # GoogLeNet
+      # google net would need to resize to be 299 insted of 224
       self.model_name = "GoogLeNet_Inception_v3"
-      model_ft = models.inception_v3(pretrained=True)
+      model_ft = models.inception_v3(pretrained=True, aux_logits=False)
+      model_ft.fc = nn.Linear(self.model.fc.in_features,4096)
       self.model = model_ft.to(self.device)  
-      # remove the dropout + linear classifier layer
-      layers = list(self.model.to(self.device).children())[:-2]
-      self.model = nn.Sequential(*layers)
+      self.linear = None
+    
 
     elif choice == "AlexNet":
       self.model_name = "AlexNet"
       model_ft = models.alexnet(pretrained=True)
-      self.model = model_ft.to(self.device)  
+      layers = list(model_ft.children())[:-1]
       # remove classifier layer
-      net = list(model_ft.children())[-1]
-      net_layers = list(net.children())[:-2]
-      net = nn.Sequential(*net_layers)
-      model_layers = list(model_ft.children())[:-1] + [net]
-      self.model = nn.Sequential(*model_layers)
+      model_ft = torch.nn.Sequential(*layers)
+      self.model = model_ft.to(self.device)  
+      self.linear = torch.nn.Linear(256*6*6,4096).to(self.device)  
 
     elif choice == "VGG-19":
       self.model_name = "VGG-19"
       model_ft = torch.hub.load('pytorch/vision:v0.10.0', 'vgg19', pretrained=True)
-
-      self.model = model_ft.to(self.device)  
+      layers = list(model_ft.children())[:-1]
       # remove classifier layer
-      net = list(model_ft.children())[-1]
-      net_layers = list(net.children())[:-2]
-      net = nn.Sequential(*net_layers)
-      model_layers = list(model_ft.children())[:-1] + [net]
-      self.model = nn.Sequential(*model_layers)
+      model_ft = nn.Sequential(*layers)
+      self.model = model_ft.to(self.device)
+      self.linear = nn.Linear(512*7*7,4096).to('cuda') 
     
 
     # Freeze parameters so we don't backprop through them
@@ -70,8 +66,9 @@ class ImageEncoder(nn.Module):
   def forward(self, X):
     # think about the ensemble and dropout a bit more
     features = self.model(X)
-    if self.dropout:
-        features = self.dropout(features)
+    if self.linear:
+      features_flatten = features.flatten()
+      features = self.linear(features_flatten)
     return features
 
   def encode_image(self, image_path, normalize=False):
@@ -85,8 +82,7 @@ class ImageEncoder(nn.Module):
       self.model.to('cuda')
 
     if not self.trained:
-      print("Model has not been trained")
-      return -1
+      print("Model has not been trained/finetuned")
     with torch.no_grad():
       output = self.model(img_processed)[0]
     if normalize:
